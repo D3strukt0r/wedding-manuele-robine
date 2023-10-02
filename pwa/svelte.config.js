d65b53e5c956de03eeceb54a581bb16cf86872ae
D3strukt0r <dev@d3strukt0r.me>
2023-10-02 17:01:28 +0200

    Add security headers

--- pwa/svelte.config.js
+++ pwa/svelte.config.js
@@ -11,8 +11,28 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter()
-  }
+    adapter: adapter(),
+
+    // https://kit.svelte.dev/docs/configuration#csp
+    csp: {
+      directives: {
+        'default-src': ['none'],
+        'script-src': ['self'],
+        'connect-src': ['self'],
+        'img-src': ['self'],
+        'manifest-src': ['self'],
+        'style-src': ['self', 'unsafe-inline'],
+        'font-src': ['self'],
+        'base-uri': ['self'],
+        'form-action': ['self'],
+        'frame-ancestors': ['none'],
+      },
+      // Must be specified with either the `report-to` or `report-uri` directives, or both
+      // reportOnly: {
+      //   'script-src': ['self'],
+      // },
+    },
+  },
 };
 
 export default config;
