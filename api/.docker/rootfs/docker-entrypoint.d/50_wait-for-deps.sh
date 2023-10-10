b18db3c0e25810e16de8811bdd03da44ab620bd1
D3strukt0r <dev@d3strukt0r.me>
2023-10-11 01:29:43 +0200

    Instead of netcat "nc" use db client for ping

--- api/.docker/rootfs/docker-entrypoint.d/50_wait-for-deps.sh
+++ api/.docker/rootfs/docker-entrypoint.d/50_wait-for-deps.sh
@@ -1,8 +1,68 @@
 #!/bin/bash
 set -e -u -o pipefail
 
-WAIT_TIMEOUT=${WAIT_TIMEOUT:=600}
-WAIT_FOR=${WAIT_FOR:=db:3306}
+# Check if DATABASE_URL is set
+if [[ -z "${DATABASE_URL:-}" ]]; then
+  entrypoint_error 'DATABASE_URL is not set'
+  exit 1
+fi
 
-entrypoint_info "Waiting up to $WAIT_TIMEOUT seconds for remote port \"$WAIT_FOR\" ..."
-wait-for.sh -t $WAIT_TIMEOUT $WAIT_FOR
+# https://stackoverflow.com/a/17287984/4156752
+# extract proto/scheme
+driver="`echo $DATABASE_URL | grep '://' | sed -e's|^\(.*\)://.*|\1|g'`"
+url=`echo $DATABASE_URL | sed -e s,$driver://,,g`
+
+# extract the user and password (if any)
+userpass="`echo $url | grep @ | cut -d@ -f1`"
+password=`echo $userpass | grep : | cut -d: -f2`
+if [ -n "$password" ]; then
+    user=`echo $userpass | grep : | cut -d: -f1`
+else
+    user=$userpass
+fi
+
+# extract the host -- updated
+hostport=`echo $url | sed -e s,$userpass@,,g | cut -d/ -f1`
+port=`echo $hostport | grep : | cut -d: -f2`
+if [ -n "$port" ]; then
+    host=`echo $hostport | grep : | cut -d: -f1`
+else
+    host=$hostport
+fi
+
+# extract the path (if any)
+database="`echo $url | grep / | cut -d/ -f2- | cut -d? -f1`"
+
+DEPS_TIMEOUT=${DEPS_TIMEOUT:=60}
+
+entrypoint_info "Waiting up to $DEPS_TIMEOUT seconds for remote db \"$driver://$user:$password@$host:$port/$database\" ..."
+
+# https://stackoverflow.com/a/70362046/4156752
+start_time="$(date -u +%s)"
+current_time="$(date -u +%s)"
+elapsed_seconds=$(($current_time - $start_time))
+
+if [[ $driver == 'mysql' ]]; then
+  until [ $elapsed_seconds -gt $DEPS_TIMEOUT ] || mysql --host="$host" --port="$port" --user="$user" --password="$password" -e "SELECT 1" >/dev/null 2>&1; do
+    current_time="$(date -u +%s)"
+    elapsed_seconds=$(($current_time - $start_time))
+    entrypoint_warn "Still waiting for db to be ready... Or maybe the db is not reachable. $(($elapsed_seconds - $DEPS_TIMEOUT)) seconds left"
+    sleep 1
+  done
+elif [[ $driver == 'pgsql' ]]; then
+  until [ $elapsed_seconds -gt $DEPS_TIMEOUT ] || pg_isready --host="$host" --port="$port" --username="$user" --dbname="$database" >/dev/null 2>&1; do
+    current_time="$(date -u +%s)"
+    elapsed_seconds=$(($current_time - $start_time))
+    entrypoint_warn "Still waiting for db to be ready... Or maybe the db is not reachable. $(($elapsed_seconds - $DEPS_TIMEOUT)) seconds left"
+    sleep 1
+  done
+else
+  entrypoint_error 'Database not supported! Use either MySQL or PostgreSQL'
+  exit 1
+fi
+
+if [ $elapsed_seconds -gt $DEPS_TIMEOUT ]; then
+  entrypoint_error 'The db is not up or not reachable'
+else
+  entrypoint_info 'The db is now ready and reachable'
+fi
