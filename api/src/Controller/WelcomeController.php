5d4bc936ef09de2e68ab66ad8d6cfe88ad83b5a5
D3strukt0r <dev@d3strukt0r.me>
2023-10-16 00:22:44 +0200

    Add welcome page

---
+++ api/src/Controller/WelcomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class WelcomeController extends AbstractController
+{
+    #[Route('/welcome', name: 'welcome')]
+    public function __invoke(): Response
+    {
+        return $this->render('welcome.html.twig');
+    }
+}
