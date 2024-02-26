d066e23b7af4b996eef3d8cdaedd42eb56ab1ee2
D3strukt0r <dev@d3strukt0r.me>
2024-02-26 19:25:13 +0100

    Use Symfony Decorators for MigrationFactory

--- api/src/Migrations/Factory/MigrationFactoryDecorator.php
+++ api/src/Migrations/Factory/MigrationFactoryDecorator.php
@@ -5,6 +5,8 @@ namespace App\Migrations\Factory;
 use App\Migrations\AbstractMigrationWithCommand;
 use Doctrine\Migrations\AbstractMigration;
 use Doctrine\Migrations\Version\MigrationFactory;
+use Symfony\Component\DependencyInjection\Attribute\AsDecorator;
+use Symfony\Component\DependencyInjection\Attribute\AutowireDecorated;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
@@ -12,9 +14,11 @@ use Symfony\Component\HttpKernel\KernelInterface;
  *
  * @link https://symfony.com/bundles/DoctrineMigrationsBundle/current/index.html#migration-dependencies
  */
+#[AsDecorator('doctrine.migrations.migrations_factory')]
 readonly class MigrationFactoryDecorator implements MigrationFactory
 {
     public function __construct(
+        #[AutowireDecorated]
         private MigrationFactory $migrationFactory,
         private KernelInterface $kernel,
     ) {}
