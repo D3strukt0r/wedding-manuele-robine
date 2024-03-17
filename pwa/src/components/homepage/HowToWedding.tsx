d8518b61dbfe74c119a9bd34ce3397f39fa36eb8
D3strukt0r <dev@d3strukt0r.me>
2024-03-17 11:22:11 +0100

    Fix aligning menu section

--- pwa/src/components/homepage/HowToWedding.tsx
+++ pwa/src/components/homepage/HowToWedding.tsx
@@ -13,9 +13,9 @@ export default function HowToWedding({id}: {id?: string}) {
             alt="Bild von Manuele & Robine"
             className="w-full md:shadow-[-1rem_1rem_0_0_#dab4a7]"
           />
-          <h2 className="absolute bottom-0 text-gray-50 uppercase text-3xl mx-10 mb-6 md:hidden philosopher-regular">{t('homepage.howToWedding.title1')}</h2>
+          <h2 className="absolute bottom-0 text-gray-50 uppercase text-3xl mx-8 mb-6 md:hidden philosopher-regular">{t('homepage.howToWedding.title1')}</h2>
         </div>
-        <div className="flex-1 px-4 py-8 md:mr-16 md:mt-12">
+        <div className="flex-1 px-8 md:pl-0 py-8 md:mr-16 md:mt-12">
           <h2 className="text-gray-50 uppercase text-3xl mb-6 hidden md:block philosopher-regular">{t('homepage.howToWedding.title1')}</h2>
           <h2 className="text-gray-50 uppercase text-3xl mb-6 md:hidden philosopher-regular">{t('homepage.howToWedding.title2')}</h2>
           <p className="whitespace-pre-line text-gray-50 noto-sans-regular">{t('homepage.howToWedding.text')}</p>
--- pwa/src/components/homepage/MenuAndSelection.tsx
+++ pwa/src/components/homepage/MenuAndSelection.tsx
@@ -75,58 +75,65 @@ export default function ManuAndSelection({id}: {id?: string}) {
   // });
 
   return (
-    <div className="bg-red-dark md:pr-20">
-      <div
-        id={id}
-        className="flex flex-col md:flex-row-reverse bg-red-light"
-      >
-        <div className="flex-1">
-          <img
-            src={menu}
-            alt="Bild von Essen"
-            className="md:mt-12 md:translate-x-12 w-full md:shadow-[-1rem_1rem_0_0_#403a37]"
-          />
-        </div>
-        <div className="flex-1 m-8">
-          <h2 className="uppercase text-3xl mb-6 philosopher-regular">{t('homepage.menu.title')}</h2>
-          <div className="md:hidden">
-            {menuOptions.map((item, index) => (
-              <Disclosure
-                as="div"
-                key={item.key}
-                className={clsx('border-b-[1px] border-gray-dark', {'border-t-[1px]': index === 0})}
-              >
-                {({open}) => (
-                  <>
-                    <Disclosure.Button className="py-2">
-                      <FontAwesomeIcon
-                        icon={faChevronDown}
-                        className={clsx(
-                          'mr-4 transform transition-transform motion-reduce:transition-none',
-                          {'rotate-180': open}
-                        )}
-                      />
-                      <span className="text-lg philosopher-regular">{item.title}</span>
-                    </Disclosure.Button>
-                    <Disclosure.Panel className="whitespace-pre-line ml-8 noto-sans-regular">
-                      {item.text}
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
-            ))}
+    <div className="bg-gradient-to-r from-45% from-red-light to-55% to-red-dark">
+      <div className="mx-auto max-w-7xl md:px-6 md:pr-20 lg:px-8 lg:pr-32">
+        <div className="bg-red-light">
+          <div
+            id={id}
+            className="flex flex-col md:flex-row-reverse"
+          >
+            <div className="flex-1">
+              <img
+                src={menu}
+                alt="Bild von Essen"
+                className="md:mt-12 md:translate-x-12 w-full md:shadow-[-1rem_1rem_0_0_#403a37]"
+              />
+            </div>
+            <div className="flex-1 m-8 md:mx-0">
+              <h2 className="uppercase text-3xl mb-6 philosopher-regular">{t('homepage.menu.title')}</h2>
+              <div className="md:hidden">
+                {menuOptions.map((item, index) => (
+                  <Disclosure
+                    as="div"
+                    key={item.key}
+                    className={clsx('border-b-[1px] border-gray-dark', {'border-t-[1px]': index === 0})}
+                  >
+                    {({open}) => (
+                      <>
+                        <Disclosure.Button className="py-2">
+                          <FontAwesomeIcon
+                            icon={faChevronDown}
+                            className={clsx(
+                              'mr-4 transform transition-transform motion-reduce:transition-none',
+                              {'rotate-180': open}
+                            )}
+                          />
+                          <span className="text-lg philosopher-regular">{item.title}</span>
+                        </Disclosure.Button>
+                        <Disclosure.Panel className="whitespace-pre-line ml-8 noto-sans-regular">
+                          {item.text}
+                        </Disclosure.Panel>
+                      </>
+                    )}
+                  </Disclosure>
+                ))}
+              </div>
+              <div className="hidden md:block">
+                <h3 className="text-2xl mb-4 philosopher-regular">{t('homepage.menu.meat.title')}</h3>
+                <p className="whitespace-pre-line noto-sans-regular">{t('homepage.menu.meat.text')}</p>
+                <h3 className="text-2xl mb-4 philosopher-regular">{t('homepage.menu.vegetarian.title')}</h3>
+                <p className="whitespace-pre-line noto-sans-regular">{t('homepage.menu.vegetarian.text')}</p>
+              </div>
+            </div>
           </div>
-          <div className="hidden md:block">
-            <h3 className="text-2xl mb-4 philosopher-regular">{t('homepage.menu.meat.title')}</h3>
-            <p className="whitespace-pre-line noto-sans-regular">{t('homepage.menu.meat.text')}</p>
-            <h3 className="text-2xl mb-4 philosopher-regular">{t('homepage.menu.vegetarian.title')}</h3>
-            <p className="whitespace-pre-line noto-sans-regular">{t('homepage.menu.vegetarian.text')}</p>
+          <div className="bg-red-light md:pt-8">
+            <div className="px-8 pb-8">
+              <QrScannerCheck
+                ref={qrRef}
+                onScan={handleScan}
+              />
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="bg-red-light md:pt-8">
-        <div className="px-8 pb-8">
-          <QrScannerCheck ref={qrRef} onScan={handleScan} />
         </div>
       </div>
     </div>
