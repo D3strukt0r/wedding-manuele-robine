cd6e5d33bebe9c238e8a80891d208a5eb32f3e27
D3strukt0r <dev@d3strukt0r.me>
2024-04-17 21:46:56 +0200

    Add Dashboard layout

--- pwa/src/components/admin/Dashboard.tsx
+++ pwa/src/components/admin/Dashboard.tsx
@@ -1,3 +1,35 @@
+import AdminLayout, {AdminLayoutProps} from '../common/admin/AdminLayout.tsx';
+import {faHouse} from '@fortawesome/free-solid-svg-icons';
+import {useContext} from 'react';
+import AuthenticationContext from '../../context/AuthenticationContext.tsx';
+import {useTranslation} from 'react-i18next';
+
 export default function Dashboard() {
-  return undefined;
+  const {t} = useTranslation("app");
+  const [authentication, updateAuthentication] = useContext(AuthenticationContext);
+
+  const navigation = [
+    {
+      name: t('admin.menu.dashboard'),
+      href: '#',
+      icon: faHouse,
+      current: false,
+    },
+  ] satisfies AdminLayoutProps['navigation'];
+  const userNavigation = [
+    {
+      name: t('menu.logout'),
+      onClick: () => {
+        updateAuthentication(null);
+      },
+    },
+  ] satisfies AdminLayoutProps['userNavigation'];
+
+  return (
+    <AdminLayout
+      navigation={navigation}
+      userNavigation={userNavigation}
+      user={authentication?.username}
+    />
+  );
 }
