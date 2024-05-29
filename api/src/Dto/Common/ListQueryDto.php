82ea2b36df1cbc617cb1beaa8af41a4bcafabfa6
D3strukt0r <dev@d3strukt0r.me>
2024-05-29 19:25:08 +0200

    Improve list Dto

--- api/src/Dto/Common/ListQueryDto.php
+++ api/src/Dto/Common/ListQueryDto.php
@@ -2,24 +2,32 @@
 
 namespace App\Dto\Common;
 
+use OpenApi\Attributes as OA;
 use Symfony\Component\Validator\Constraints as Assert;
 
 readonly class ListQueryDto
 {
     public function __construct(
         #[Assert\LessThanOrEqual(500)]
-        public int $limit = 500, // TODO: Reset to 25
+        #[OA\Property(description: 'The field used to limit the number of records returned', example: 10)]
+        public int $limit = 25,
 
         #[Assert\LessThanOrEqual(10_000)]
+        #[OA\Property(description: 'The field used to offset the records returned', example: 20)]
         public int $offset = 0,
 
         #[Assert\NotBlank(allowNull: true)]
+        #[OA\Property(description: 'Global search through all searchable columns', example: 'John Doe')]
         public ?string $search = null,
 
         /** @var array<ListSortersDto> */
+        #[Assert\Valid]
+        #[OA\Property(description: 'Sort by certain columns', example: [['property' => 'name', 'direction' => 'asc']])]
         public array $sorters = [],
 
         /** @var array<ListFiltersDto> */
+        #[Assert\Valid]
+        #[OA\Property(description: 'Filter certain columns', example:[['property' => 'name', 'value' => 'John Doe']])]
         public array $filters = [],
     ) {}
 }
