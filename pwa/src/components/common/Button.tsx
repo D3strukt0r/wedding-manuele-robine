bd0981351a0f2d4dda3a2592163b475ee36133d9
D3strukt0r <dev@d3strukt0r.me>
2024-11-08 23:16:34 +0700

    Use lighter green for buttons and radius like in design

--- pwa/src/components/common/Button.tsx
+++ pwa/src/components/common/Button.tsx
@@ -22,9 +22,9 @@ const Button = forwardRef<HTMLButtonElement, Props>(({
         'inline-flex justify-center items-center text-sm leading-6 px-3 py-2 shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2',
         props.className,
         {
-          'font-noto-sans': layout === 'app-primary',
+          'font-noto-sans rounded-lg': layout === 'app-primary',
           'font-semibold rounded-md': layout !== 'app-primary',
-          'bg-app-green-dark text-white hover:bg-app-gray-dark focus-visible:outline-app-green-dark': !disabled && layout === 'app-primary',
+          'bg-app-green text-white hover:bg-app-green-dark focus-visible:outline-app-green-dark': !disabled && layout === 'app-primary',
           'bg-blue-600 hover:bg-blue-500 text-white focus-visible:outline-blue-600': !disabled && layout === 'primary',
           'bg-white hover:bg-gray-50 text-gray-900 ring-1 ring-inset ring-gray-300 ': !disabled && layout === 'secondary',
           'bg-red-600 hover:bg-red-500 text-white focus-visible:outline-red-600': !disabled && layout === 'danger',
